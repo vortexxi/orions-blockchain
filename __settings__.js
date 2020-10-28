d40b7571004f188594a8a616497380f0ea9c86d6
Vortex <Aram@mbp-de-vortex-2.lan>
2020-10-28 17:40:41 +0100

    ORIONS project, vertical menu

---
+++ __settings__.js
@@ -0,0 +1,20 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1014174.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 36945472, 36945473, 36945474, 36945475, 36997998, 36998135, 37185318, 37185345, 37185396 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
